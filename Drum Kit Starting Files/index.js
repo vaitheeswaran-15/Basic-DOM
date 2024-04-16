85411b5ed8b13c7948d60c46d926906e5cba25e0
Vaitheeswaran L M <hachari15@gmail.com>
2024-04-16 22:39:47 +0530

    Added Key listner

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,37 +4,45 @@ for (var i = 0; i < buttonLength; i++)
 {
     document.querySelectorAll('.drum')[i].addEventListener('click', function(){
       var value = this.innerHTML;
-      switch (value){
-        case 'w': 
-        var tom = new Audio("sounds/tom-1.mp3");
-        tom.play();
-        break;
-        case 'a':
-        var tom = new Audio("sounds/tom-2.mp3");
-        tom.play();
-        break;
-        case 's':
-        var tom = new Audio("sounds/tom-3.mp3");
-        tom.play();
-        break;
-        case 'd':
-        var tom = new Audio("sounds/tom-4.mp3");
-        tom.play();
-        break;
-        case 'j':
-        var tom = new Audio("sounds/crash.mp3");
-        tom.play();
-        break;
-        case 'k':
-        var tom = new Audio("sounds/kick-bass.mp3");
-        tom.play();
-        break;
-        case 'l':
-        var tom = new Audio("sounds/snare.mp3");
-        tom.play();
-        break;
-        default:
-            console.log(value);
-      }  
+      playAudio(value);
     })
+}
+
+
+document.addEventListener('keydown',function(event){
+  playAudio(event.key);
+})
+function playAudio(value){
+  switch (value){
+    case 'w': 
+    var tom = new Audio("sounds/tom-1.mp3");
+    tom.play();
+    break;
+    case 'a':
+    var tom = new Audio("sounds/tom-2.mp3");
+    tom.play();
+    break;
+    case 's':
+    var tom = new Audio("sounds/tom-3.mp3");
+    tom.play();
+    break;
+    case 'd':
+    var tom = new Audio("sounds/tom-4.mp3");
+    tom.play();
+    break;
+    case 'j':
+    var tom = new Audio("sounds/crash.mp3");
+    tom.play();
+    break;
+    case 'k':
+    var tom = new Audio("sounds/kick-bass.mp3");
+    tom.play();
+    break;
+    case 'l':
+    var tom = new Audio("sounds/snare.mp3");
+    tom.play();
+    break;
+    default:
+        console.log(value);
+  } 
 }
